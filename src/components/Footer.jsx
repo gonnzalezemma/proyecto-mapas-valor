954c43fc33e41539d84bdb467bff2d67b5778b2b
juanosorioh <juangabrielosorioheredia@gmail.com>
2022-09-09 12:09:33 -0300

    links en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div className="container">  <footer className="py-3 my-4">
-    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
-    </ul>
+    <div className="row justify-content-center border-bottom pb-3 mb-3">
+      <div className='col-auto'>
+        <Link to="">Home</Link>
+      </div>
+      <div className='col-auto'>
+        <Link to="Register">Contact</Link>
+      </div>
+      <div className='col-auto'>
+        <Link to="Login">About</Link>
+      </div>
+    </div>
     <p className="text-center text-muted">&copy; 2022 Company, Inc</p>
   </footer>
 </div>
