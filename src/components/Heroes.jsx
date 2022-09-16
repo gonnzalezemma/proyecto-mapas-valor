9b267e8d3afa2b7f94ea3fad4dbf4ff05f5a2c62
juanosorioh <juangabrielosorioheredia@gmail.com>
2022-09-16 11:11:31 -0300

    detalles en home

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -5,11 +5,13 @@ const Heroes = (props) => {
   return (
     <>
       {" "}
+
       <div className="container">
         <div className="row">
           <div className="col-md-">
             <div className="container col-xxl-8 px-4 py-5">
               <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <h1 className="text-center display-5 fw-bold lh-1 mb-3">inicio</h1>
                 <div className="col-10 col-sm-8 col-lg-6">
                   <img src={mapa} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
                 </div>
