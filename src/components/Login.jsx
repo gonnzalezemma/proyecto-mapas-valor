31befa045577a83007bed2701b583b409d40fbf7
juanosorioh <juangabrielosorioheredia@gmail.com>
2022-10-26 09:57:53 -0300

    prueba

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ export default function LoginComponent() {
                   Si no esta registrado{" "}
                   <Link to='/Register' style={{ color: "black" }}>
                     {" "}
-                    Registrese aqui aasdsads
+                    Registrese aqui prueba nueva
                   </Link>
                 </p>
               </div>
