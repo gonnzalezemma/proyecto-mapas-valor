f8e467845572ece977de3928258e7d9bba14d1a0
gonnzalezemma <jorge_99.l@hotmail.com>
2022-11-04 12:05:24 -0300

    add mapPoint in routing

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -4,7 +4,10 @@ import { NotFound } from "../pages/NotFound";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Perfil } from "../pages/Perfil";
-import { Maps } from "../pages/Maps";
+/* import { SplitMaps } from "../pages/PointMap";
+import { HeatMap } from "../pages/heatMap"; */
+import { PointMap } from "../pages/PointMap";
+
 
 export default function Routing() {
   return (
@@ -14,7 +17,7 @@ export default function Routing() {
       <Route path='login' element={<Login />} />
       <Route path='*' element={<NotFound />} />
       <Route path='perfil' element={<Perfil />} />
-      <Route path='maps' element={<Maps />} />
+      <Route path='maps' element={<PointMap />} />
     </Routes>
   );
 }
