1f3d8d8dc403e4d9a0e8f64560d5855489eec894
santiR22 <santiagorodas688@gmail.com>
2022-11-11 09:26:27 -0300

    configuracion de las rutas del bakcend en variables de entorno

---
+++ src/env/env.js
@@ -0,0 +1,5 @@
+export const login = import.meta.env.VITE_BACKEND_LOGIN;
+export const get_users = import.meta.env.VITE_BACKEND_GET_USER;
+export const create_user = import.meta.env.VITE_BACKEND_CREATE_USER;
+export const delete_user = import.meta.env.VITE_BACKEND_DELETE_USER;
+export const update_user = import.meta.env.VITE_BACKEND_UPDATE_USER;
