05bde78ddd12131ed4efcae67c9379c5afda9b5f
gonnzalezemma <jorge_99.l@hotmail.com>
2022-11-17 15:08:10 -0300

    add style

--- src/pages/Mapstore.jsx
+++ src/pages/Mapstore.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Mapstore() {
   return (
     <>
-  <iframe allowFullScreen style={{border: "none"}}  height="700px" width="95%" src="http://localhost:8082/mapstore/#/context/Mapa-interactivof-fsa"></iframe>
+  <iframe allowFullScreen style={{border: "none", display:"inline-table", height:"85vh", width:"100%"}}  src="http://localhost:8082/mapstore/#/context/Mapa-interactivof-fsa"></iframe>
     </>
   )
 }
